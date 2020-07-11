dd733aebdf5463545dec829d81d5f1055de50458
Priyanshu <priyanshu.gandhi12@nmims.edu.in>
2020-07-11 10:51:43 +0530

    Updated settings file

--- news/settings.py
+++ news/settings.py
@@ -117,4 +117,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
 
-django_heroku.setting(locals())
+django_heroku.settings(locals())
