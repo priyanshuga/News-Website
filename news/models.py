41eed2f906fe9c2098ed3e3286f0e0363baad60c
Priyanshu <priyanshu.gandhi12@nmims.edu.in>
2020-07-19 21:44:46 +0530

    eror 4

--- news/models.py
+++ news/models.py
@@ -34,7 +34,7 @@ class News(models.Model):
     category = models.CharField(choices=CATEGORY_CHOICES , max_length=13 , default=0)
     created = models.DateTimeField(default=timezone.now)
     reviews_tag = models.CharField(choices=REVIEWS_CHOICES, max_length=6, default=0)
-    #keyword = models.CharField(max_length=1000)
+    keyword = models.CharField(max_length=1000)
     slug = models.SlugField(max_length=255,unique=True)
 
     
