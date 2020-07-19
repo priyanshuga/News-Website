23aadeeea257ab7bfeda01db4273bb0d945c12c6
Priyanshu <priyanshu.gandhi12@nmims.edu.in>
2020-07-19 20:18:06 +0530

    eror 1

--- news/models.py
+++ news/models.py
@@ -21,7 +21,7 @@ REVIEWS_CHOICES = (
 )
 
 class News(models.Model):
-    head = models.CharField(max_length=100)
+    head = models.CharField(max_length=60)
     head_desc = models.CharField(max_length=150)
     title = models.CharField(max_length=500)
     short_desc = models.CharField(max_length=1000)
@@ -34,7 +34,7 @@ class News(models.Model):
     category = models.CharField(choices=CATEGORY_CHOICES , max_length=13 , default=0)
     created = models.DateTimeField(default=timezone.now)
     reviews_tag = models.CharField(choices=REVIEWS_CHOICES, max_length=6, default=0)
-    keyword = models.CharField(max_length=1000)
+    #keyword = models.CharField(max_length=1000)
     slug = models.SlugField(max_length=50,unique=True)
 
     
